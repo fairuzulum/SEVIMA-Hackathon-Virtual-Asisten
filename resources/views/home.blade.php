0aed4d752e0527ad57eb08fb4548a01526b7710a
fairuzulum <usleunmdi32@gmail.com>
2023-06-24 13:04:36 +0700

    Ubah Judul

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 
 @section('container')
       <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex justify-content-center align-items-center" style="height: 700px;">
-    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Learning Today,<br>Leading Tomorrow</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
-      <a href="courses.html" class="btn-get-started">Get Started</a>
-    </div>
-  </section><!-- End Hero -->
+      <section id="hero" class="d-flex justify-content-center align-items-center" style="height: 700px;">
+        <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+            <h1>Belajar dengan Asisten Virtual</h1>
+            <h2>Meningkatkan Pembelajaran, Mendorong Prestasi</h2>
+            <a href="courses.html" class="btn-get-started">Mulai Sekarang</a>
+        </div>
+    </section><!-- End Hero -->
 
 
 
