2b80b41f2d10089edb91186b4799021bbbeb8931
fairuzulum <usleunmdi32@gmail.com>
2023-06-24 12:55:16 +0700

    menambah halaman home

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,19 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">SEVIMA</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="active" href="#">Home</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#" class="get-started-btn">Login</a>
+
+    </div>
+  </header><!-- End Header -->
